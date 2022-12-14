c36e93d646e788be10beda3efb4db6e94d491392
tariqyunusa <tariikhyunusa@gmail.com>
2022-12-14 09:54:26 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print sign - Prints the sign of a number
+ * print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
