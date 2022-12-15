495bdda81d2a862d19188947b6ff165e82086bdb
tariqyunusa <tariikhyunusa@gmail.com>
2022-12-15 12:44:36 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: The numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x  = 0; x <= 9; x++0)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+}
