90c1b8257e5f03b9da9e693ad9b4dcbdfc23ce45
tariqyunusa <tariikhyunusa@gmail.com>
2022-12-15 13:34:51 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 
 /**
@@ -10,10 +11,10 @@ void print_numbers(void)
 {
 	int x;
 
-	for (x  = 0; x <= 9; x++0)
+	for (x = 0; x <= 9; x++0)
 	{
-		_putchar(x + '0');
+		putchar(x + '0');
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
+
 /**
  * print_most_numbers - print the numbers from 0 to 9
  *
  * Description: printsnumbers except 2 and 4
- *
  * Return: The numbers from 0 to 9
  */
 void print_most_numbers(void)
