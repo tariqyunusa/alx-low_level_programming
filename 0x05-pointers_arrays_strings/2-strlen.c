369fb3c76bc33e2e4ecff3a334a97ae669d9dbf7
tariqyunusa <tariikhyunusa@gmail.com>
2022-12-20 11:23:21 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,15 @@
 #include "main.h"
+
 /**
  * strlen - return the length of a string
  * @a: input
+ * Return: length of the string
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int a;
 
-	strlen(a);
+	for (a = 0; s[a] != '\0' ; a++);
+
+	return (a);
 }
