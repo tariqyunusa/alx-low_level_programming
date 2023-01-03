e3670baa13d91f9ae3f2173c0d79f16d87f55a8e
tariqyunusa <tariikhyunusa@gmail.com>
2023-01-03 14:21:24 -0800

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard.
+ * @: array of pointers.
+ *
+ * Return: void.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <=7; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar(10);
+	}
+}
