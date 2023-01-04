9fad9a15d243cffb6923c48859d28a3fa78417a5
tariqyunusa <tariikhyunusa@gmail.com>
2023-01-04 11:24:03 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
-#include "main.h""
+#include "main.h"
+#include <stdio.h>
 /**
  * _print_rev_recursion - prints a string in reverse.
  * @s: pointer to string.
@@ -10,6 +11,6 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		putchar(*s);
 	}
 }
