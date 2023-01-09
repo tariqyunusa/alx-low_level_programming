680e0a0ce333570a69847c6c84c2e017a8e0dd72
tariqyunusa <tariikhyunusa@gmail.com>
2023-01-08 23:53:10 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that prints its name, followed by a new line.
+ * if you rename the prigram, it will print the new name, without having to compile it again.
+ * you should not remove the path before the name of the program
+ *
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ *
+ * Return: This to 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s/n", argv[0]);
+	(void)argc;
+	return(0);
+}
