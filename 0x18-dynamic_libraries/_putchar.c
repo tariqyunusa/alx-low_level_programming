e1183d7ad9a66b2a469e2a9ee32b1f7477e1cea6
tariqyunusa <tariikhyunusa@gmail.com>
2023-03-21 04:28:18 -0700

    _putchar file

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Printing __putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("_putchar\n");
+
+	return (0);
+
+}
